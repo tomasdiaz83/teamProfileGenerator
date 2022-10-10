95fb908f82b4173dd9b47c7e4a86abf639edc8a6
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-10-09 20:33:30 -0500

    Classes created with corresponding tests passing

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,65 @@
+const Employee = require('../lib/Employee');
+
+describe("Employee class", () => {
+    describe("Employee properties", () => {
+        it('All employees should have a name', () => {
+            // Arrange
+            const employee1 = new Employee("John", "1089673", "email@here.com")
+            // Act
+            // Assert
+            expect(employee1.name).toBe("John");
+        })
+        it('All employees should have an id', () => {
+            // Arrange
+            const employee1 = new Employee("John", "1089673", "email@here.com")
+            // Act
+            // Assert
+            expect(employee1.id).toBe("1089673");
+        })
+        it('All employees should have an email', () => {
+            // Arrange
+            const employee1 = new Employee("John", "1089673", "email@here.com")
+            // Act
+            // Assert
+            expect(employee1.email).toBe("email@here.com");
+        })
+    })
+
+    describe("Employee methods", () => {
+        it('get the name of each employee', () => {
+            //Arrange
+            const employee1 = new Employee("John", "1089673", "email@here.com")
+            //Act
+            const name = employee1.getName();
+            //Assert
+            expect(name).toBe('John');
+        })
+
+        it('get the ID of each employee', () => {
+            //Arrange
+            const employee1 = new Employee("John", "1089673", "email@here.com")
+            //Act
+            const id = employee1.getId();
+            //Assert
+            expect(id).toBe('1089673');
+        })
+
+        it('get the email of each employee', () => {
+            //Arrange
+            const employee1 = new Employee("John", "1089673", "email@here.com")
+            //Act
+            const email = employee1.getEmail();
+            //Assert
+            expect(email).toBe('email@here.com');
+        })
+
+        it('get the role of each employee', () => {
+            //Arrange
+            const employee1 = new Employee("John", "1089673", "email@here.com")
+            //Act
+            const role = employee1.getRole();
+            //Assert
+            expect(role).toBe('Employee');
+        })
+    })
+});
---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,84 @@
+const Engineer = require('../lib/Engineer');
+
+describe("Engineer class", () => {
+    it('There should be only one engineer', () => {
+        
+    })
+    describe("Engineer properties", () => {
+        it('Engineer should have a name', () => {
+            // Arrange
+            const engineer = new Engineer ("John", "1089673", "email@here.com", "tester83");
+            // Act
+            // Assert
+            expect(engineer.name).toBe("John");
+        })
+        it('Engineer should have an id', () => {
+            // Arrange
+            const engineer = new Engineer ("John", "1089673", "email@here.com", "tester83");
+            // Act
+            // Assert
+            expect(engineer.id).toBe("1089673");
+        })
+        it('Engineer should have an email', () => {
+            // Arrange
+            const engineer = new Engineer ("John", "1089673", "email@here.com", "tester83");
+            // Act
+            // Assert
+            expect(engineer.email).toBe("email@here.com");
+        })
+        it('Engineer should have an office number', () => {
+            //Arrange
+            const engineer = new Engineer ("John", "1089673", "email@here.com", "tester83");
+            //Act
+            //Assert
+            expect(engineer.gitHub).toBe("tester83");
+        })
+    })
+
+    describe("Employee methods", () => {
+        it('get the name of engineer', () => {
+            //Arrange
+            const engineer = new Engineer ("John", "1089673", "email@here.com", "tester83");
+            //Act
+            const name = engineer.getName();
+            //Assert
+            expect(name).toBe('John');
+        })
+
+        it('get the ID of engineer', () => {
+            //Arrange
+            const engineer = new Engineer ("John", "1089673", "email@here.com", "tester83");
+            //Act
+            const id = engineer.getId();
+            //Assert
+            expect(id).toBe('1089673');
+        })
+
+        it('get the email of engineer', () => {
+            //Arrange
+            const engineer = new Engineer ("John", "1089673", "email@here.com", "tester83");
+            //Act
+            const email = engineer.getEmail();
+            //Assert
+            expect(email).toBe('email@here.com');
+        })
+
+        it('get the role of engineer', () => {
+            //Arrange
+            const engineer = new Engineer ("John", "1089673", "email@here.com", "tester83");
+            //Act
+            const role = engineer.getRole();
+            //Assert
+            expect(role).toBe('Engineer');
+        })
+
+        it('get the gitHub of engineer', () => {
+            //Arrange
+            const engineer = new Engineer ("John", "1089673", "email@here.com", "tester83");
+            //Act
+            const gitHub = engineer.getGitHub();
+            //Assert
+            expect(gitHub).toBe("tester83");
+        })
+    })
+});
