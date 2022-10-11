ea6c39db3ab3b59e57511cf7f337926479a11207
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-10-11 15:12:29 -0500

    Redoing file organization to remove the createTeam class from the mix

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const questions = require('./lib/inquirerQuestions')
+const inquirer = require('inquirer');
 
-
-function init () {
-    createManager();
-    createEngineer();
+let team = {
+    Manager: "",
+    Engineers: [],
+    Interns: []
 }
 
-init()
+//Prompts user to create a a Manager
+function createManager(team) {
+    return inquirer
+        .prompt(questions.Manager)
+        .then(answers => {
+            //Adds manager to the members object
+            team.Manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+            console.log(team);
+        })
+}
+
+//Prompts user to create an employee
+function createEmployee () {
+    return inquirer.prompt(questions.Employee)
+    .then((data) => {
+        return data;
+    })
+}
+
+//Prompts user to get the team
+function getTeam () {
+    return console.log(this.members);
+}
+
+createManager(team)
