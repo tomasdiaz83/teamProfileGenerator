60904bc67583caf389873afe5a6246120d149be6
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-10-11 12:57:33 -0500

    createEmployee is not working

--- _tests_/createTeam.test.js
+++ _tests_/createTeam.test.js
@@ -1,32 +1,32 @@
-// const CreateTeam = require('../lib/createTeam');
-// const inquirer = require('inquirer');
+const CreateTeam = require('../lib/createTeam');
+const inquirer = require('inquirer');
 
-// jest.mock('inquirer')
+jest.mock('inquirer')
 
-// describe("CreateTeam", () => {
-//     it("should create an empty object", () => {
-//         //arrange
-//         const team = new CreateTeam;
-//         //const x = {}
-//         //act
-//         //assert
-//         expect(team.team).toEqual({})
-//     })
-//     describe("createManager", () => {
-//         it('should add a Manager to the team object', () => {
-//             // Arrange
-//             const testTeam = new CreateTeam;
-//             // Act
-//             testTeam.createManager.inquirer.prompt.mockReturnValue(
-//                 answers = {
-//                     name : "John",
-//                     id : "1089673",
-//                     email : "test@testing.com",
-//                     officeNumber : "2418"
-//                 }
-//             )
-//             // Assert
-//             expect(testTeam.team.manager).resolves("John");
-//         })
-//     })
-// })
+describe("CreateTeam", () => {
+    it("should create an empty object", () => {
+        //arrange
+        const team = new CreateTeam;
+        //const x = {}
+        //act
+        //assert
+        expect(team.team).toEqual({})
+    })
+    describe("createManager", () => {
+        it('should add a Manager to the team object', () => {
+            // Arrange
+            const testTeam = new CreateTeam;
+            // Act
+            testTeam.createManager.inquirer.mockReturnValue(
+                answers = {
+                    name : "John",
+                    id : "1089673",
+                    email : "test@testing.com",
+                    officeNumber : "2418"
+                }
+            )
+            // Assert
+            expect(testTeam.team.manager).resolves("John");
+        })
+    })
+})
